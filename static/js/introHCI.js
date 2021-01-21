4f7d38f62aaaec87ea3eebce9f8d46ea13eee7a1
HermesKeng <kaining1101@gmail.com>
2021-01-20 21:02:39 -0800

    Practice Jquery on lab3

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -1,18 +1,38 @@
-'use strict';
+"use strict";
 
 // Call this function when the page loads (the "ready" event)
-$(document).ready(function() {
-	initializePage();
-})
+$(document).ready(function () {
+  initializePage();
+});
 
 /*
  * Function that is called when the document is ready.
  */
-function initializePage() {
-	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
-	});
 
-	// Add any additional listeners here
-	// example: $("#div-id").click(functionToCall);
+function projectClick(e) {
+  e.preventDefault();
+  var containingProject = $(this).closest(".project");
+  var description = $(containingProject).find(".project-description");
+  if (description.length != 0) {
+    description.html(
+      "<div class='project-description'><p>Stop Clicking on me you just did it </p>" +
+        new Date() +
+        "</div>"
+    );
+  } else {
+    containingProject.append(
+      "<div class='project-description'><p>Description of the project</p></div>"
+    );
+  }
+}
+
+function initializePage() {
+  $("#testjs").click(function (e) {
+    $(".jumbotron h1").text("Javascript has taken control");
+    $("#testjs").text("Please Wait....");
+    $(".jumbotron p").toggleClass("active");
+  });
+  $(".project").click(projectClick);
+  // Add any additional listeners here
+  // example: $("#div-id").click(functionToCall);
 }
